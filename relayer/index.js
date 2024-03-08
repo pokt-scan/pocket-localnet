85c71f34ca71788021da4dbac13bac139fb805ae
Jorge S. Cuesta <jckpo2@gmail.com>
2024-03-08 17:01:34 -0400

    Refactored relayer to handle everything from a configuration file. Added relayer as part of docker-compose.yml and as dependency of mesh

--- relayer/index.js
+++ relayer/index.js
@@ -1,108 +1,176 @@
-import Piscina from "piscina";
-import * as Stream from "stream";
-import bunyan from "bunyan";
+import Piscina from 'piscina'
+import * as Stream from 'stream'
+import fs from 'fs'
+import bunyan from 'bunyan'
+import bFormat from 'bunyan-format'
+import joi from 'joi'
+import _ from 'lodash'
+
+const dataSchema = joi.object({
+  rpcUrls: joi.array().min(1).items(joi.string()).required(),
+  whitelistedServicers: joi.array().optional().default([]),
+  appPrivKey: joi.string().hex().required(),
+  chain: joi.object().keys({
+    id: joi.string().required(),
+    callSet: joi.array().min(1).items(joi.object().keys({
+      method: joi.string().optional().default(null).allow(''),
+      path: joi.string().optional().default(null).allow(''),
+      headers: joi.object().optional().unknown(true),
+      data: joi.string().required(),
+    })).required(),
+  }).required(),
+  mesh: joi.object().keys({
+    enabled: joi.bool().default(false),
+    rpcUrl: joi.string().when('enabled', {
+      is: true,
+      then: joi.string().required(),
+      otherwise: joi.string().optional().allow(''),
+    }),
+  }).optional().default({ enabled: false }),
+  relays: joi.number().min(1).default(1),
+  logRelayData: joi.bool().default(false).optional(),
+  logFile: joi.string().optional().default('relays.log').allow(''),
+})
+
+const bunyanFormat = bFormat({ outputMode: 'short', levelInString: true })
 
 const logger = bunyan.createLogger({
-    name: 'relayer',
-    level: 'debug',
-    streams: [{
-        level: 'info',
-        stream: process.stdout // log INFO and above to stdout
-    }, {
-        type: 'rotating-file',
-        path: './logs/relays.log',
-        period: '1d',   // daily rotation
-        count: 3        // keep 3 back copies
-    }]
-});
+  name: 'relayer',
+  level: 'debug',
+  stream: bunyanFormat,
+})
 
-const run = () => {
-    return new Promise((resolve, reject) => {
-        const pool = new Piscina({
-            filename: new URL('./worker.mjs', import.meta.url).href,
-            maxQueue: 'auto'
-        });
+const readDataFile = async () => {
+  const filePath = _.isEmpty(process.env.DATA_PATH) ? './data.json' : process.env.DATA_PATH
 
-        const tasks = [];
-        const totalRelays = Number(process.env.MAX_RELAYS || 1000)
+  if (!fs.existsSync(filePath)) {
+    throw new Error(`file ${filePath} does not exist`)
+  }
 
-        const stream = new Stream.Readable()
-        stream.setEncoding('utf8');
+  const data = await fs.readFileSync(filePath)
 
-        pool.on('drain', () => {
-            if (stream.isPaused()) {
-                logger.info('resuming...', pool.queueSize);
-                stream.resume();
-            }
-        });
+  logger.info(`reading ${filePath} file`)
 
-        stream
-            .on('data', (data) => {
-                tasks.push(pool.run(data));
-                if (pool.queueSize === pool.options.maxQueue) {
-                    logger.info('pausing...', pool.queueSize);
-                    stream.pause();
-                }
-            })
-            .on('error', (e) => {
-                reject(e)
-            })
-            .on('end', async () => {
-                const results = await Promise.allSettled(tasks)
+  return JSON.parse(data)
+}
 
-                let goodRelays = 0
-                let goodRelaysTime = 0
-                let badRelays = 0
-                const relaysByNode = {}
+const run = (data) => {
+  return new Promise((resolve, reject) => {
+    const validation = dataSchema.validate(data)
+    if (validation.error) {
+      reject(validation.error)
+      return
+    }
 
-                results.forEach(p => {
-                    if (p.status === 'rejected' || !p.value.success) {
+    const logToFile = _.isBoolean(data.logRelayData) && data.logRelayData
 
-                        badRelays++
-                        if (p.status === 'rejected') {
-                            logger.error(`relay promise reject because: ${p.reason}`)
-                        } else if (p.value && p.value.success) {
-                            logger.error(`relay promise fulfilled but was a bad relay because: ${p.value.errorMsg}`)
-                        }
-                    } else {
-                        goodRelays++
-                        goodRelaysTime += p.value.relayTime
-                        if (relaysByNode[p.value.node]) relaysByNode[p.value.node]++
-                        else relaysByNode[p.value.node] = 1
-                        if (p.value.responseSample && p.value.responseSample.length > 0) {
-                            const items = p.value.responseSample
-                            logger.debug('response', {
-                                node: p.value.node,
-                                response: items,
-                            });
-                        }
-                    }
-                })
+    if(logToFile) logger.level('debug')
 
-                logger.info('=======================================================================')
-                logger.info(`Good Relays: ${goodRelays}`)
-                logger.info(`Relays MS Avg: ${goodRelays > 0 ? goodRelaysTime / goodRelays : 0}`)
-                logger.info(`Bad Relays: ${badRelays}`)
-                logger.info(JSON.stringify(relaysByNode, null, 2))
-                logger.info('=======================================================================')
-
-                setTimeout(() => {
-                    resolve()
-                }, 1000)
-            });
-
-        for( let i=0; i < totalRelays; i++) {
-            stream.push(`${i}`)
-        }
-
-        // finish
-        stream.push(null)
+    logger.addStream({
+      type: 'rotating-file',
+      path: data.logFile,
+      level: 'debug',
+      // daily rotation
+      period: '1d',
+      // keep 3 back copies
+      count: 3,
     })
+
+    const pool = new Piscina({
+      filename: new URL('./worker.mjs', import.meta.url).href,
+      maxQueue: 'auto',
+      workerData: data,
+    })
+
+    const tasks = []
+
+    const stream = new Stream.Readable()
+    stream.setEncoding('utf8')
+
+    pool.on('drain', () => {
+      if (stream.isPaused()) {
+        logger.info({ queueSize: pool.queueSize }, 'resuming pool queue')
+        stream.resume()
+      }
+    })
+
+    stream
+      .on('data', () => {
+        tasks.push(pool.run())
+        if (pool.queueSize === pool.options.maxQueue) {
+          logger.info({ queueSize: pool.queueSize }, 'pausing pool queue')
+          stream.pause()
+        }
+      })
+      .on('error', (e) => {
+        reject(e)
+      })
+      .on('end', async () => {
+        const results = await Promise.allSettled(tasks)
+
+        let goodRelays = 0
+        let goodRelaysTime = 0
+        let badRelays = 0
+        const relaysByNode = {}
+
+        logger.info('analyzing results', {relays: data.relays})
+        results.forEach(p => {
+          if (p.status === 'rejected' || !p.value.success) {
+
+            badRelays++
+            if (p.status === 'rejected') {
+              logger.error({err: p.reason}, `relay rejected`)
+            } else if (p.value && p.value.success) {
+              logger.error({err: p.value.errorMsg, hash: p.value.debug.hash}, `relay success with error`)
+            }
+          } else {
+            goodRelays++
+            goodRelaysTime += p.value.relayTime
+            if (relaysByNode[p.value.node]) relaysByNode[p.value.node]++
+            else relaysByNode[p.value.node] = 1
+
+            if(logToFile){
+              logger.debug('relay data', {
+                hash: p.value.debug.hash,
+                request: p.value.debug.request,
+                response: p.value.debug.response,
+              })
+            }
+          }
+        })
+
+        logger.info('summary', {
+          goodRelays,
+          badRelays,
+          byNodeCount: relaysByNode,
+          avgMs: goodRelays > 0 ? goodRelaysTime / goodRelays : 0,
+        })
+
+        // give enough time to streams complete file write on logs
+        if(logToFile) setTimeout(resolve, 2000)
+        else resolve()
+      })
+
+    for (let i = 0; i < data.relays; i++) {
+      stream.push(`${i}`)
+    }
+
+    // finish
+    stream.push(null)
+  })
+}
+
+const exec = async () => {
+  const data = await readDataFile()
+  return run(data)
 }
 
 (function () {
-    run().then(() => process.exit(0)).catch((e) => {
-        console.trace(e)
-        process.exit(1)
-    })
+  exec().then(() => {
+    logger.info('relayer job done')
+    process.exit(0)
+  }).catch((e) => {
+    logger.trace({ err: e }, 'something went wrong')
+    process.exit(1)
+  })
 })()
--- relayer/worker.mjs
+++ relayer/worker.mjs
@@ -3,83 +3,81 @@ import {JsonRpcProvider} from "@pokt-foundation/pocketjs-provider";
 import {KeyManager} from "@pokt-foundation/pocketjs-signer";
 import {PocketAAT} from "@pokt-network/aat-js";
 import _ from "lodash";
-
-const APP_PRIVATE_KEY = "6d7d9e78fd62b524cfa76a298b6f9653445449bc22960224901a5bb993ba52cb1802f4116b9d3798e2766a2452fbeb4d280fa99e77e61193df146ca4d88b38af"
-const APP_CHAIN = "0021"
-
-const LEAN_1_URL = "http://localhost:8071"
-const LEAN_2_URL = "http://localhost:8081"
-const LEAN_3_URL = "http://localhost:8091"
-const MESH_URL = "http://localhost:9081"
-const DISPATCHERS = [
-    LEAN_1_URL,
-    LEAN_2_URL,
-    LEAN_3_URL,
-]
-const RELAY_DATA = [
-    "[{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0xF02c1c8e6114b1Dbe8937a39260b5b0a374432bB\", \"latest\"],\"id\":1}]"
-]
-const NODES = {
-    "7c08e2e1265246a66d7d022b163970114dda124e": MESH_URL,
-    "9ab105b900c4633657f60974ad0e243c8f50ae1e": MESH_URL,
-    "cb85946c8171e3bbe78f5dbc01469053419b7be1": MESH_URL,
-    "5e6949faf0a176fd0f3a0e2ef948d7a70ee2867b": MESH_URL,
-    "4202057f345d63b0af02f76dcb42aa46bf9b6d43": MESH_URL,
-    "a31eba7042bd2c87c5dc0462d92dd1c961c81249": MESH_URL,
-    "d1dd513de5a3c1f05b6c534c840f76e60caf3662": MESH_URL,
-    "a4357688f25b1daa3270c287c0fbb75bb020c1ce": MESH_URL,
-    "b0c626b04d5f0ab76e764409fc9bafb6cab2c1b1": MESH_URL,
-    "b3f65b5c8da10132b107aaa1c38542ffb73dea35": MESH_URL,
-    "e441b6024deb682291abff461bf9cc855f5ae659": MESH_URL,
-    "d8f7226ec86e62739b84aaa8898d8b7b8c2e3025": MESH_URL,
-    "f89f49b6a978ddfc7402b7bd0efca8715c1d7d5e": MESH_URL,
-    "6fa859c95b450a589d1a837338c0b7ffbde6872b": MESH_URL,
-    "3c107bcbd07db3a43882fa20c41bae5904aa0677": MESH_URL,
-    "580751119d154cb508ac024bcab772e04c4714e2": MESH_URL,
-    "56f4af690d1ac39b8f4c4fb9892ede2757e94624": MESH_URL,
-    "34755f065d73a7743bf3f149660e0392b878317b": MESH_URL,
-    "621993ee115ad88682ed401e213e7b389e296832": MESH_URL,
-    "80f930617802d4496376b1663e91cafb515e21ad": MESH_URL,
-    "9c3e3919baa75d8ea4d11989f6ffc25e4190d5ce": MESH_URL,
-    "7853706d177a233401065eb09d849a77f61f153e": MESH_URL,
-    "6260f3c4306dcf88668ceb4108621459f36f0798": MESH_URL,
-    "111675de8e13fde1ce4da5fc236ab98ed478cc20": MESH_URL,
-}
+import { workerData } from 'piscina'
 
 async function initialize() {
+    const logToFile = _.isBoolean(workerData.logRelayData) && workerData.logRelayData
+
     // Instantiate a provider for querying information on the chain!
     const provider = new JsonRpcProvider({
-        rpcUrl: _.sample(DISPATCHERS),
-        // If you'll Instantiate a relayer, you need to add dispatchers as well
-        dispatchers: DISPATCHERS,
+        rpcUrl: _.sample(workerData.rpcUrls),
+        dispatchers: workerData.rpcUrls,
     });
 
     // Instantiate a signer for importing an account and signing messages!
-    const appKeyManager = await KeyManager.fromPrivateKey(APP_PRIVATE_KEY);
-
+    const appKeyManager = await KeyManager.fromPrivateKey(workerData.appPrivKey);
     const appPubKey = appKeyManager.getPublicKey();
-
     const pocketAAT = await PocketAAT.from(
-        "0.0.1",
-        appPubKey, // aat
-        appPubKey,
-        APP_PRIVATE_KEY,
+      "0.0.1",
+      appPubKey, // aat
+      appPubKey,
+      workerData.appPrivKey,
     )
 
+    // create a relayer to get a session
     const relayer = new Relayer({
         keyManager: appKeyManager,
         provider,
-        dispatchers: DISPATCHERS,
+        dispatchers: workerData.rpcUrls,
     });
 
-    return async () => {
-        let node
-        const startTime = new Date()
+    // get a session for the chain id
+    const session = await relayer.getNewSession({
+        chain: workerData.chain.id,
+        applicationPubKey: appPubKey,
+    });
 
-        const session = await relayer.getNewSession({
-            chain: APP_CHAIN,
-            applicationPubKey: appPubKey,
+    const nodes = []
+
+    session.nodes.forEach(node => {
+        let selectedNode = null
+
+        if(workerData.whitelistedServicers.length > 0) {
+            if(workerData.whitelistedServicers.includes(node.address)) {
+                selectedNode = node
+            }
+        } else {
+            selectedNode = node
+        }
+
+        if(!selectedNode) return
+
+        // if mesh is enabled the nodes url will be the one on the mesh
+        if(workerData.mesh !== null && workerData.mesh.enabled === true) {
+            selectedNode.serviceUrl = workerData.mesh.rpcUrl
+        }
+
+        nodes.push(selectedNode)
+    })
+
+    if(nodes.length === 0) {
+        throw new Error("we are unable to get at least 1 node to service relays for the app + chain right now.")
+    }
+
+    // override session nodes with the whitelisted, or they will be everyone with mesh or not.
+    session.nodes = nodes
+
+    return async () => {
+        const relayer = new Relayer({
+            keyManager: appKeyManager,
+            provider,
+            // set node url as dispatchers
+            dispatchers: session.nodes.map(n => n.serviceUrl),
         });
+
+        const rpcData = _.sample(workerData.chain.callSet)
+
+        const startTime = new Date()
 
         const analytics = {
             success: true,
@@ -87,43 +85,44 @@ async function initialize() {
             relayTime: 0,
             node: undefined,
             session: session,
-            responseSample: []
+            debug: {
+                hash: "",
+                request: logToFile ? rpcData : null,
+                response: null,
+            }
         }
 
-        let notInSession = true
-
-        while (notInSession) {
-            const sessionNode = _.sample(session.nodes)
-            if (NODES[sessionNode.address]) {
-                notInSession = false
-                sessionNode.serviceUrl = NODES[sessionNode.address]
-                node = sessionNode
+        const relay = {
+            data: rpcData.data,
+            blockchain: workerData.chain.id,
+            pocketAAT: pocketAAT,
+            session: session,
+            options: {
+                timeout: 180000,
             }
+        }
+
+        if(!_.isEmpty(rpcData.method)) {
+            relay.method = rpcData.method
+        }
+
+        if(!_.isEmpty(rpcData.path)) {
+            relay.path = rpcData.path
         }
 
         try {
-            const relay = await relayer.relay({
-                data: RELAY_DATA[0],
-                blockchain: APP_CHAIN,
-                pocketAAT: pocketAAT,
-                session: session,
-                // method: "POST",
-                // path: "/path-here",
-                node,
-                options: {
-                    timeout: 180000,
-                }
-            })
+            const relayResponse = await relayer.relay(relay)
 
-            if (relay.serviceNode) {
-                analytics.node = `${relay.serviceNode.address}@${relay.serviceNode.serviceUrl.toString()}`
+            analytics.debug.hash = relayResponse.relayProof.requestHash
+
+            if (relayResponse.serviceNode) {
+                analytics.node = `${relayResponse.serviceNode.address}@${relayResponse.serviceNode.serviceUrl.toString()}`
             }
 
-            if (relay.response !== "") {
-                analytics.responseSample.push(relay.response)
+            if (logToFile) {
+                analytics.debug.response = relayResponse.response
             }
         } catch (e) {
-            console.log("errored relay", e.message)
             analytics.success = false
             analytics.errorMsg = e.message
         } finally {
