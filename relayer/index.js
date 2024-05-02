973108117510ee622ded362b0be69f5251d9ef8e
Jorge S. Cuesta <jckpo2@gmail.com>
2024-05-02 15:53:26 -0400

    General Rework over LocalNet files. (#1)
    * Rework localnet and relayer
    * Refactored relayer to handle everything from a configuration file.
    *Added relayer as part of docker-compose.yml and as a dependency of mesh
    * Updated Readme | changed log to info level | changed localhost to node address
    * Updated mesh timeout to 80k ns | implemented write to file of relays response in relayer
    * Removed incongruent success check-in relayer
    * Added pocket core and geo-mesh max bytes config to json files.
    * Updated genesis.json.
    
    ---------
    
    Co-authored-by: Ramiro Rodriguez Colmeiro <ramiro@poktscan.com>
    Co-authored-by: Ramiro Rodriguez Colmeiro <rawthil@gmail.com>

--- relayer/index.js
+++ relayer/index.js
@@ -1,99 +1,186 @@
-import Piscina from "piscina";
-import * as Stream from "stream";
+import Piscina from 'piscina'
+import * as Stream from 'stream'
+import fs from 'fs'
+import bunyan from 'bunyan'
+import bFormat from 'bunyan-format'
+import joi from 'joi'
+import _ from 'lodash'
 
-// function sleep(ms) {
-//     console.log("info", `process sleep ${ms}ms`)
-//     return new Promise((resolve) => {
-//         setTimeout(resolve, ms);
-//     });
-// }
+const dataSchema = joi.object({
+  rpcUrls: joi.array().min(1).items(joi.string()).required(),
+  whitelistedServicers: joi.array().optional().default([]),
+  appPrivKey: joi.string().hex().required(),
+  chain: joi.object().keys({
+    id: joi.string().required(),
+    callSet: joi.array().min(1).items(joi.object().keys({
+      method: joi.string().optional().default(null).allow(''),
+      path: joi.string().optional().default(null).allow(''),
+      headers: joi.object().optional().unknown(true),
+      data: joi.string().required(),
+    })).required(),
+  }).required(),
+  mesh: joi.object().keys({
+    enabled: joi.bool().default(false),
+    rpcUrl: joi.string().when('enabled', {
+      is: true,
+      then: joi.string().required(),
+      otherwise: joi.string().optional().allow(''),
+    }),
+  }).optional().default({ enabled: false }),
+  relays: joi.number().min(1).default(1),
+  logRelayData: joi.bool().default(false).optional(),
+  logFile: joi.string().optional().default('relays.log').allow(''),
+  writeRawRelayData: joi.bool().default(false).optional(),
+  rawFile: joi.string().optional().default('relays.raw').allow(''),
+})
 
-const run = () => {
-    return new Promise((resolve, reject) => {
-        const pool = new Piscina({
-            filename: new URL('./worker.mjs', import.meta.url).href,
-            maxQueue: 'auto'
-        });
+const bunyanFormat = bFormat({ outputMode: 'long', levelInString: true })
 
-        // let round = 1
-        // const maxRounds = Number(process.env.MAX_ROUNDS || 1)
+const logger = bunyan.createLogger({
+  name: 'relayer',
+  level: 'debug',
+  stream: bunyanFormat,
+})
 
-        const tasks = [];
-        const totalRelays = Number(process.env.MAX_RELAYS || 1000)
+const readDataFile = async () => {
+  const filePath = _.isEmpty(process.env.DATA_PATH) ? './data.json' : process.env.DATA_PATH
 
-        const stream = new Stream.Readable()
-        stream.setEncoding('utf8');
+  if (!fs.existsSync(filePath)) {
+    throw new Error(`file ${filePath} does not exist`)
+  }
 
-        pool.on('drain', () => {
-            if (stream.isPaused()) {
-                console.log('resuming...', pool.queueSize);
-                stream.resume();
-            }
-        });
+  const data = await fs.readFileSync(filePath)
 
-        stream
-            .on('data', (data) => {
-                tasks.push(pool.run(data));
-                if (pool.queueSize === pool.options.maxQueue) {
-                    console.log('pausing...', pool.queueSize);
-                    stream.pause();
-                }
-            })
-            .on('error', (e) => {
-                reject(e)
-            })
-            .on('end', async () => {
-                const results = await Promise.allSettled(tasks)
+  logger.info(`reading ${filePath} file`)
 
-                let goodRelays = 0
-                let goodRelaysTime = 0
-                let badRelays = 0
-                const relaysByNode = {}
+  return JSON.parse(data)
+}
 
-                console.log("debug", "analyze results")
-                results.forEach(p => {
-                    if (p.status === 'rejected' || !p.value.success) {
+const run = (data) => {
+  return new Promise((resolve, reject) => {
+    const validation = dataSchema.validate(data)
+    if (validation.error) {
+      reject(validation.error)
+      return
+    }
+    
+    const rawToFile = _.isBoolean(data.writeRawRelayData) && data.writeRawRelayData
+    const logToFile = _.isBoolean(data.logRelayData) && data.logRelayData
 
-                        badRelays++
-                        if (p.status === 'rejected') {
-                            console.error(`relay promise reject because: ${p.reason}`)
-                        } else if (p.value && p.value.success) {
-                            console.error(`relay promise fulfilled but was a bad relay because: ${p.value.errorMsg}`)
-                        }
-                    } else {
-                        goodRelays++
-                        goodRelaysTime += p.value.relayTime
-                        if (relaysByNode[p.value.node]) relaysByNode[p.value.node]++
-                        else relaysByNode[p.value.node] = 1
-                        // if (p.value.responseSample && p.value.responseSample.length > 0) {
-                        //     const items = p.value.responseSample
-                        //     console.log(`sample response: ${items[Math.floor(Math.random()*items.length)]}`)
-                        // }
-                    }
-                })
+    if(logToFile) logger.level('debug')
 
-                console.log("info", '=======================================================================')
-                console.log("info", `Good Relays: ${goodRelays}`)
-                console.log("info", `Relays MS Avg: ${goodRelays > 0 ? goodRelaysTime / goodRelays : 0}`)
-                console.log("info", `Bad Relays: ${badRelays}`)
-                console.log("info", JSON.stringify(relaysByNode, null, 2))
-                console.log("info", '=======================================================================')
-
-                resolve()
-            });
-
-        for( let i=0; i < totalRelays; i++) {
-            stream.push(`${i}`)
-        }
-
-        // finish
-        stream.push(null)
+    logger.addStream({
+      type: 'rotating-file',
+      path: data.logFile,
+      level: 'debug',
+      // daily rotation
+      period: '1d',
+      // keep 3 back copies
+      count: 3,
     })
+
+    const pool = new Piscina({
+      filename: new URL('./worker.mjs', import.meta.url).href,
+      maxQueue: 'auto',
+      workerData: data,
+    })
+
+    const tasks = []
+
+    const stream = new Stream.Readable()
+    stream.setEncoding('utf8')
+
+    pool.on('drain', () => {
+      if (stream.isPaused()) {
+        logger.info({ queueSize: pool.queueSize }, 'resuming pool queue')
+        stream.resume()
+      }
+    })
+
+    stream
+      .on('data', () => {
+        tasks.push(pool.run())
+        if (pool.queueSize === pool.options.maxQueue) {
+          logger.info({ queueSize: pool.queueSize }, 'pausing pool queue')
+          stream.pause()
+        }
+      })
+      .on('error', (e) => {
+        reject(e)
+      })
+      .on('end', async () => {
+        const results = await Promise.allSettled(tasks)
+
+        let goodRelays = 0
+        let goodRelaysTime = 0
+        let badRelays = 0
+        const relaysByNode = {}
+
+        logger.info('analyzing results', {relays: data.relays})
+        results.forEach(p => {
+          if (p.status === 'rejected' || !p.value.success) {
+
+            badRelays++
+            if (p.status === 'rejected') {
+              logger.error({err: p.reason}, `relay rejected`)
+            } else if (p.value) {
+              logger.error({err: p.value.errorMsg, hash: p.value.debug.hash}, `relay success with error`)
+            }
+          } else {
+            goodRelays++
+            goodRelaysTime += p.value.relayTime
+            if (relaysByNode[p.value.node]) relaysByNode[p.value.node]++
+            else relaysByNode[p.value.node] = 1
+
+            if(logToFile){
+              logger.debug('relay data', {
+                hash: p.value.debug.hash,
+                request: p.value.debug.request,
+                response: p.value.debug.response,
+              })
+            }
+            if(rawToFile){
+              fs.appendFile(data.rawFile, p.value.debug.response + '\n', (err) => {
+                if (err) {
+                  console.error('Error writing log to file:', err);
+                }
+              });
+            }
+          }
+        })
+
+        logger.info('summary', {
+          goodRelays,
+          badRelays,
+          byNodeCount: relaysByNode,
+          avgMs: goodRelays > 0 ? goodRelaysTime / goodRelays : 0,
+        })
+
+        // give enough time to streams complete file write on logs
+        if(logToFile) setTimeout(resolve, 2000)
+        else resolve()
+      })
+
+    for (let i = 0; i < data.relays; i++) {
+      stream.push(`${i}`)
+    }
+
+    // finish
+    stream.push(null)
+  })
+}
+
+const exec = async () => {
+  const data = await readDataFile()
+  return run(data)
 }
 
 (function () {
-    run().then(() => process.exit(0)).catch((e) => {
-        console.trace(e)
-        process.exit(1)
-    })
+  exec().then(() => {
+    logger.info('relayer job done')
+    process.exit(0)
+  }).catch((e) => {
+    logger.trace({ err: e }, 'something went wrong')
+    process.exit(1)
+  })
 })()
